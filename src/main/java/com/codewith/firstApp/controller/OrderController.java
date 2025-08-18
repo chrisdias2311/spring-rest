b892e5294ff887dc6f37d95df4b61bf93380427b
Chris Dias <105737889+chrisdias2311@users.noreply.github.com>
2025-08-18 15:55:46 +0530

    Created first REST Req (#1)
    Created a Simple REST API

---
+++ src/main/java/com/codewith/firstApp/controller/OrderController.java
@@ -0,0 +1,13 @@
+package com.codewith.firstApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+    @GetMapping("/order")
+    public String order(@RequestParam String item){
+        return "Your order for " + item + " has been received!";
+    }
+}

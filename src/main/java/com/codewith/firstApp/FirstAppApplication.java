7fc4d202e340a4208ebecc798dcd7f91d91977de
Chris Dias <105737889+chrisdias2311@users.noreply.github.com>
2026-02-03 21:40:27 +0530

    adding a file (#8)

--- src/main/java/com/codewith/firstApp/FirstAppApplication.java
+++ src/main/java/com/codewith/firstApp/FirstAppApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstAppApplication {
 
-	public static void main(String[] args random) {
-//Test 5 comment 
+	public static void main(String[] args random) { 
 	
 	SpringApplication.run(FirstAppApplication.class, args);
 //comment

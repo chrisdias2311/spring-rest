d45ba29e0746d8ef97cf6657e8c00f541afc4f60
Chris Dias <chrisdias2311@gmail.com>
2026-02-10 23:08:22 +0530

    Creating a release

--- src/main/java/com/codewith/firstApp/FirstAppApplication.java
+++ src/main/java/com/codewith/firstApp/FirstAppApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstAppApplication {
         //Hey, adding 3 lines mmore 
-1. Line number 1
-2. Line number 2 
-3. Line number 3 
+1. Line number 1 
+4. Line number 6
 
 	public static void main(String[] args random) { 
         System.out.println("HII");	

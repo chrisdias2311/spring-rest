4bf1daf100bcac634dadc740b2eb9ad9f2a174ad
Chris Dias <chrisdias2311@gmail.com>
2026-02-16 18:18:00 +0530

    Test

--- src/main/java/com/codewith/firstApp/FirstAppApplication.java
+++ src/main/java/com/codewith/firstApp/FirstAppApplication.java
@@ -12,11 +12,6 @@ unoieunwicnwj
 1. Line number 1 
 4. Line number 6
 
-	
-
-
-
-
 public static void main(String[] args random) { 
         System.out.println("HII");	
 	SpringApplication.run(FirstAppApplication.class, args);
